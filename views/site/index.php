dbd7f370f74e88084309e1d8fa6105d845d605d2
Gustavo G. Andrade <gugoan@hotmail.com>
2016-08-21 20:30:12 -0300

    fix index

--- views/site/index.php
+++ views/site/index.php
@@ -8,27 +8,28 @@ $this->title = 'Sicoob Crediriodoce - Cana de Ética';
     <div class="jumbotron">
         <h1>Canal de Ética </h1>
 
-Este é um canal exclusivo para relato de situações de descumprimento do Código de Ética do Sicoob Crediriodoce, de forma confidencial e, se desejável anônima. Se este for o caso clique em "Relatar Incidente" ou em "Acompanhar Incidente" neste site.
+<p class="text-justify">Este é um canal exclusivo para relato de situações de descumprimento do Código de Ética do Sicoob Crediriodoce, de forma confidencial e, se desejável anônima. Se este for o caso clique em <?= Html::a('"Relatar Incidente"', ['/occurrence/create']) ?> ou em <?= Html::a('"Acompanhar Incidente"', ['/occurrence/search']) ?> neste site.
 
 Para outras finalidades, o Sicoob Crediriodoce disponibiliza os seguintes canais de comunicação:
+</p>
 
 <div class="row">
   <div class="col-md-6">
 
 <address>
-  <strong>Central de Relacionamento</strong><br>
+  <h3>Central de Relacionamento</h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> –  0800 642 0000 - Central de Atendimento<br>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> –  0800 702 0756 - Cartões Sicoobcard <br>
 </address>
 
 <address>
-  <strong>SAC SICOOB </strong><br>
+  <h3>SAC Sicoob</h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 0800 642 0000<br>
 <small>Dúvidas relacionadas ao uso dos canais de autoatendimento. Funciona 24 horas, todos os dias da semana.</small>
 </address>
 
 <address>
-  <strong>SAC BANCOOB </strong><br>
+  <h3>SAC Bancoob </h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>– 0800 724 4420<br>
 <small>Poupança, Previdência, Crédito Consignado INSS, Privado e Servidor Público, Procapcred, Cotas-Partes, Fundos de Investimento, além de boletos fraudados e rastreamento de cheques.</small>
 </address>
@@ -37,21 +38,21 @@ Para outras finalidades, o Sicoob Crediriodoce disponibiliza os seguintes canais
   <div class="col-md-6">
 
 <address>
-  <strong>SAC SICOOB CONSÓRCIOS</strong><br>
+  <h3>SAC Sicoob Consórcios</h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 0800 607 3 636 (demais regiões) ou<br> 
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 4007 1905 (capitais e regiões metropolitanas).<br>
 <small>Para dúvidas e informações específicas sobre o produto.</small>
 </address>
 
 <address>
-  <strong>SAC CARTÕES</strong><br>
+  <h3>SAC Cartões</h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 0800 702 0756 (demais regiões) ou <br>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 007 1256 (regiões metropolitanas).<br>
 <small>Dúvidas, roubo ou perda do cartão. Atendimento 24 horas, sete dias por semana</small>
 </address>
 
 <address>
-  <strong>Ouvidoria</strong><br>
+  <h3>Ouvidoria</h3>
 <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> – 0800 725 0996<br>
 </address>
 
@@ -60,7 +61,7 @@ Para outras finalidades, o Sicoob Crediriodoce disponibiliza os seguintes canais
 
 <p class="text-justify">Aqui você deve descrever qual foi a situação em que houve o descumprimento. É importante que o relato seja completo e detalhado, pois isso tornará a apuração da denúncia mais precisa e rápida. Não se esqueça de incluir no relato:</p>
 
-<ul>
+<ul class="text-justify">
   <li>O quê (descrição da situação)</li>
   <li>Quem (nome e sobrenome das pessoas envolvidas, inclusive testemunhas)</li>
   <li>Quando (data em que aconteceu, acontece ou acontecerá a situação)</li>
@@ -69,17 +70,21 @@ Para outras finalidades, o Sicoob Crediriodoce disponibiliza os seguintes canais
   <li>Provas (se elas existem e onde podemos encontrá-las)</li>
 </ul>
 
-Para garantir a independência no processo de apuração, se pertinente, indique no seu relato o envolvimento direto ou indireto das pessoas abaixo relacionadas:
+<p class="text-justify">
+Para garantir a independência no processo de apuração, se pertinente, indique no seu relato o envolvimento direto ou indireto das pessoas abaixo relacionadas:</p>
 
-Membros do Comitê de Ética
-    - Um Representante do Conselho de Administração: Cantídio Carlos de França ou Décio Chaves
-    - Um Representante dos Controles Internos: Kelen Mendes ou Bianca Daré
-    - Um Representante da Classe dos Colaboradores, eleito pelos mesmos em eleição democrática: Marco Antônio ou Nicolina Martins
-    - Um Representante da Diretoria: Silas Dias ou Celso Mól
-    - Um Representante do Gestão de Pessoas: Fernanda Froes ou Marina Pereira
+<h3>Membros do Comitê de Ética</h3>
+<ul class="text-justify">
+  <li>Um Representante do Conselho de Administração: Cantídio Carlos de França ou Décio Chaves</li>
+  <li>Um Representante dos Controles Internos: Kelen Mendes ou Bianca Daré</li>
+  <li>Um Representante da Classe dos Colaboradores, eleito pelos mesmos em eleição democrática: Marco Antônio ou Nicolina Martins</li>
+  <li>Um Representante da Diretoria: Silas Dias ou Celso Mól</li>
+  <li>Um Representante do Gestão de Pessoas: Fernanda Froes ou Marina Pereira</li>
+</ul>
 
+<p class="text-justify">
 Para acompanhar o andamento de sua denúncia, guarde o número do protocolo que lhe será fornecido após o registro do incidente. O prazo para resposta da sua denúncia é de até 15 dias úteis.
-Obrigado pela iniciativa e confiança.
+Obrigado pela iniciativa e confiança.</p>
     
 
     <p>
