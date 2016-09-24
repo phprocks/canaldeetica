f6af8d61b3380289d4ba893394e644b90fc88141
Gustavo G. Andrade <gugoan@hotmail.com>
2016-09-24 10:43:21 -0300

    fix index

--- views/site/index.php
+++ views/site/index.php
@@ -8,34 +8,13 @@ $this->title = 'Sicoob Crediriodoce - '.Yii::$app->params['appName'];
     <div class="jumbotron">
         <h1><?= Yii::$app->params['appName'];?></h1>
 <hr/>
-<p class="text-justify">O Canal do Colaborador tem a finalidade de atuar como canal de comunicação entre a cooperativa e seus empregados e terceirizados, sendo possível o registro de denúncias, reclamações, elogios, sugestões e outros.</p>
+<p class="text-justify">O Canal do Colaborador tem a finalidade de atuar como canal de comunicação entre a cooperativa e seus empregados, sendo possível o registro de denúncias, elogios, reclamações, sugestões e outros.</p>
 
-<p class="text-justify">Sendo que as manifestações podem ser de forma confidencial e, se desejável anônima. Se este for o caso clique em "Relatar Ocorrência" ou em "Acompanhar Ocorrência" neste site.
+<p class="text-justify">Para acompanhar o andamento de sua manifestação, guarde o número do protocolo que lhe será fornecido após o registro da ocorrência. O número de protocolo gerado é aleatório impossibilitando que outro usuário tenha acesso à manifestação.</p>
 
-Você deve descrever o máximo de informações possível para que o relato seja completo e detalhado, isso tornará a apuração da manifestação mais precisa e rápida. Não se esqueça de incluir no relato:</p>
+<p class="text-justify">O prazo para resposta é de até 15 (quinze) dias úteis.</p>
 
-<ul class="text-justify">
-  <li>O quê (descrição da situação)</li>
-  <li>Quem (nome e sobrenome das pessoas envolvidas, inclusive testemunhas)</li>
-  <li>Quando (data em que aconteceu, acontece ou acontecerá a situação)</li>
-  <li>Por quê (a causa ou motivo)</li>
-  <li>Quanto (se for possível medir)</li>
-  <li>Provas (se elas existem e onde podemos encontrá-las)</li>
-</ul>
-
-<p class="text-justify">Para garantir a independência no processo de apuração, se pertinente, indique no seu relato o envolvimento direto ou indireto das pessoas abaixo relacionadas, visto que serão elas que irão apurar o registro das manifestações:</p>
-
-<h3>Membros do Comitê de Ética</h3>
-<p>
-<ul class="text-justify">
-  <li>Um Representante do Conselho de Administração: Cantídio Carlos de França ou Décio Chaves</li>
-  <li>Um Representante dos Controles Internos: Kelen Mendes ou Bianca Daré</li>
-  <li>Um Representante da Classe dos Colaboradores, eleito pelos mesmos em eleição democrática: Marco Antônio ou Nicolina Martins</li>
-  <li>Um Representante da Diretoria: Silas Dias ou Celso Mól</li>
-  <li>Um Representante do Gestão de Pessoas: Fernanda Froes ou Marina Pereira</li>
-</ul></p>
-
-<p class="text-justify">Para acompanhar o andamento de sua denúncia, guarde o número do protocolo que lhe será fornecido após o registro do incidente. O prazo para resposta da sua denúncia é de até 15 dias úteis. Obrigado pela iniciativa e confiança.</p>
+<p class="text-justify">Caso queira que sua manifestação seja anônima, basta não preencher os campos de identificação.</p>
 
     <p>
     <?= Html::a('<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Relatar Ocorrência', ['/occurrence/create'], ['class'=>'btn btn-success']) ?>
